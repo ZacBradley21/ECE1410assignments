a26ac8afa7534ad717769cb4e51a03a039e11efe
zacbradley21 <zacbradley21@gmail.com>
2022-01-24 17:48:57 -0700

    second commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,20 @@
+/*
+Program Header
+Function Title: Main
+Summary: This program prints Hello World to the screen
+Inputs: None
+Outputs: None
+**********************
+PSEUDOCODE
+Begin
+	Use cout to print hello world
+	Return 0 to end the program
+End
+*/
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
